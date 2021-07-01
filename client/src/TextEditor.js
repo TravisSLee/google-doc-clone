ac77bc5753a18cd2c65fa5a0c20a97d0d2d92ad3
TravisSLee <travisslee223@gmail.com>
2021-07-01 19:44:09 -0400

    update edit bar

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -1,17 +1,20 @@
 import React from 'react'
-import { useEffect, useRef } from "react"
+import { useCallback } from "react"
 import "quill/dist/quill.snow.css"
 import Quill from "quill"
 
 export default function TextEditor() {
-    const wrapperRef = useRef()
-    
-
-    useEffect(() => {
-        const editor = document
+    const wrapperRef = useCallback(wrapper => {
+        if (wrapper == null) return
+        
+        wrapper.innerHTML = ""
+        const editor = document.createElement("div")
         wrapperRef.current.append(editor)
         new Quill(editor, {theme: "snow"})
     }, [])
+    
+
+    
     
     return (
         <div id="container" ref={wrapperRef}>
